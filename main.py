3ed1a93b5c4b8f1840caad03f511010ee77d2d6c
Wimfke <somers.wim@gmail.com>
2026-01-21 15:40:34 +0100

    CH1: L5: User Input

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=prompt,
+        contents=args.user_prompt,
     )
     if not response.usage_metadata:
         raise RuntimeError("No usage metadata returned from Gemini API")
 
-    print(f"User prompt: {prompt}")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
